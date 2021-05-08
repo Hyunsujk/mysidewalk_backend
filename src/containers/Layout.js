efcfaafe90298e070f3101e6797a5b36a2ba8d1e
Hyunsu Kim <hyunsujk@gmail.com>
2021-05-07 21:47:35 -0500

    choose file button works

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,5 +1,44 @@
+import { useState } from "react";
+import { Button, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  input: {
+    display: "none",
+  },
+  customButton: {
+    backgroundColor: "#05677e",
+    color: "#fff",
+  },
+});
+
 const Layout = () => {
-  return <div>layout</div>;
+  const { input, customButton } = useStyles();
+  const [file, setFile] = useState(null);
+
+  const handleUploadedFile = (e) => {
+    console.log("handle", e.target);
+
+    console.log("uploaded file", e.target.files[0]);
+
+    setFile(e.target.files[0]);
+  };
+
+  return (
+    <>
+      <input
+        accept=".txt"
+        id="file-input"
+        type="file"
+        // className={input}
+        onChange={handleUploadedFile}
+      />
+      <label htmlFor="file-input">
+        <Button variant="contained" className={customButton} component="span">
+          Choose File
+        </Button>
+      </label>
+    </>
+  );
 };
 
 export default Layout;
